257944fd063666c0f503ca2dfa1c66c71e8c52a6
Peter Forret <peter@forret.com>
2020-04-19 01:38:16 +0200

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,10 @@
 <!-- Footer-->
 <footer class="bg-light py-5">
-    <div class="container"><div class="small text-center text-muted">Copyright © 2020 - @yield('site_author',env('SITE_AUTHOR'))</div></div>
+    <div class="container"><div class="small text-center text-muted"><i class="far fa-lightbulb"></i> 2020 <i class="fas fa-user"></i> Peter Forret
+            <i class="fas fa-arrow-alt-circle-right"></i> <a href="https://nuuz.io">nuuz.io</a>
+            <i class="fas fa-arrow-alt-circle-right"></i> <a href="https://toolstud.io">toolstud.io</a>
+            <i class="fas fa-arrow-alt-circle-right"></i> <a href="https://blog.forret.com">blog.forret.com</a>
+        </div></div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -10,4 +14,13 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
 <!-- Core theme JS-->
 <script src="js/scripts.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117748093-8"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-117748093-8');
+</script>
 </html>
